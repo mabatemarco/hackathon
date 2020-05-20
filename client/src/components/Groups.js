f1cdbf8d877985c42ff4444ad98d22501bf8ef6c
Rafael Castano <rafa328@gmail.com>
2020-05-19 22:01:26 -0400

    OneGroup and Groups Components updated

--- client/src/components/Groups.js
+++ client/src/components/Groups.js
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import '../css/Groups.css';
+import OneGroup from './OneGroup';
 import { getAllGroups } from '../services/apihelper.js';
 
 
-export default function Groups(props) {
-  // Initializing allGroupsData in State with a value of 'null'
-  const [allGroupsData, setAllGroupsData] = useState(null);
+class Groups extends Component {
+  state = {
+    allGroupsData: null
+  }
 
   // making an axios call to DB to get allGroupsData then reassigning the value in state
-  useEffect(async () => {
-    const allGroupsData = await getAllGroups();
-    return setAllGroupsData(allGroupsData);
-  }, [])
-
-  return (
-    props.allGroupsData.map(group => {
-      return (
-        <div className="group-list">
-          <Group
-            title={group.title}
-            description={group.description}
-            image={group.image}
-            private={group.private}
-          />
-        </div>
-      )
+  componentDidMount = async () => {
+    const groupsData = await getAllGroups();
+    this.setState({
+      allGroupsData: groupsData
     })
-  )
+  }
+
+
+  render() {
+    return (this.state.allGroupsData &&
+      this.state.allGroupsData.map((onegroup) => {
+        return (
+          <div>
+            <OneGroup
+              className={'group-list'}
+              id={onegroup.id}
+              title={onegroup.title}
+              description={onegroup.description}
+              imageURL={onegroup.image}
+              private={onegroup.private}
+            />
+          </div>
+        )
+      })
+    )
+  }
 }
+
+export default Groups;
